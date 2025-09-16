29273136e1283cc9b20125dcf2f2c302d30d9656
Wasif <rwasif280@gmail.com>
2025-09-16 21:49:31 +0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -55,17 +55,15 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <div className="h-16 flex items-center">
+<div className="h-16 flex items-center">
   <Image
     src="/logo.jpeg"
     alt="Griffin's Property Care Logo"
-    width={0}
-    height={0}
-    sizes="100vw"
-    className="h-full w-auto object-contain"
+    width={200}   // control width
+    height={64}   // match navbar height (h-16 = 64px)
+    className="object-cover"
   />
 </div>
-
           {/* Desktop Navigation */}
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
