6a0fe1ef01ececc58985862712c46aa5ba81997a
Wasif <rwasif280@gmail.com>
2025-09-17 01:19:14 +0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,7 +57,7 @@ export function Navbar() {
           {/* Logo */}
           <div className="h-16 flex items-center">
             <Image
-              src="/logo1.png"
+              src="/logo.jpeg"
               alt="Griffin's Property Care Logo"
               width={0}
               height={0}
